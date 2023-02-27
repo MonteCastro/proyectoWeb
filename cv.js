b621a95c4c2e87294d9fcd69992a8380084b2739
MonteCastro <123337170+MonteCastro@users.noreply.github.com>
2023-02-27 12:49:26 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,9 @@
+
+ 
+  
+  function apareCer(y)
+    {if (y.style.display = "none") 
+    { y.style.display = "block";}};
+    function desAparecer(a,b,c,d)
+     {a.style.display="none";b.style.display="none";
+    c.style.display="none";d.style.display="none"}
